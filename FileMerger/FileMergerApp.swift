e3c8e54e74f16efe5c83d592b1a7d9cfd971d241
Nik Galkin <53794828+thegalkin@users.noreply.github.com>
2023-11-23 19:41:08 +0300

    Initial Commit

---
+++ FileMerger/FileMergerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileMergerApp.swift
+//  FileMerger
+//
+//  Created by Никита Галкин on 11/23/23.
+//
+
+import SwiftUI
+
+@main
+struct FileMergerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
